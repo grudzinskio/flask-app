c686194a7696965204366d2e219d57ae3a394052
Oliver Grudzinski <grudzinskioliver@gmail.com>
2026-09-21 14:49:57 -0500

    enhance flask app

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello MSOE'
+    return 'Hello MSOE, but now enhanced!'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
